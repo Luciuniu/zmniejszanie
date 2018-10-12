b648c4a37fc8cd7e70da1d7b83220872d262b1d3
Luciuniu <luciuniu@wp.pl>
2018-10-12 15:05:48 +0200

    utworzylem porownanie

---
+++ zmniejszanie.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a = 5;
+    int b = 5;
+
+    double c = 0.00000001;
+    double d = 0.000000000001;
+    double eps = 0.001;
+
+    printf("%i\n", a ==b);
+    printf("%i\n", (c - d) * (c - d) < eps);
+
+    return 0;
+}
